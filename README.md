4db6ffc1f048435fedc77ab0bb712b7db0bd36f3
Bùi Thành Nhân <hi@imnhan.com>
2020-01-01 17:17:42 +0700

    read config, generate access token

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # WIP Google Drive video support for mpv
 
 ```sh
+# Need luasec lib to send https requests
+sudo pacman -S lua52-sec
+
+mkdir -p ~/.config/mpv/scripts ~/.config/mpv/script-opts
 ln -s "$PWD/gdrive.lua" "$HOME/.config/mpv/scripts/gdrive.lua"
-TOKEN='<gdrive API token>'
-FILE_ID='<gdrive file ID>'
+cp ./gdrive.conf ~/.config/mpv/script-opts/gdrive.conf
+
+export FILE_ID='<gdrive file ID>'
 mpv "gdrive://$FILE_ID"
 ```
--- gdrive.lua
+++ gdrive.lua
@@ -1,4 +1,51 @@
-TOKEN = os.getenv("TOKEN")
+TOKEN = ""
+OPTIONS = {}
+
+-----------------------------
+-- This script registers 2 hooks:
+--   + on_load as the main entrypoint
+--   + on_load_fail in case current access token expires and needs refreshing
+-----------------------------
+
+function on_load_hook()
+    print("Loading GDrive script")
+    local url = mp.get_property("path", "")
+    if (url:find("gdrive://") == 1) then
+
+        print("Detected gdrive:// protocol, rewriting url")
+        url = "https://www.googleapis.com/drive/v3/files/" .. url:sub(10) .. "?alt=media"
+        mp.set_property("stream-open-filename", url)
+
+        OPTIONS = load_options()
+        TOKEN = get_access_token(OPTIONS)
+        set_auth_headers(TOKEN)
+
+        mp.add_hook("on_load_fail", 50, on_load_fail_hook)
+    else
+        print("Not gdrive:// protocol, doing nothing.")
+    end
+
+end
+
+function on_load_fail_hook()
+    -- In case token expires, refresh it
+    print('Token failed, refreshing...')
+    TOKEN = get_access_token(OPTIONS)
+    set_auth_headers(TOKEN)
+end
+
+-- Apparently hooks are bad and unsupported but ytdl_hook uses them.
+-- The alternative, register_event('start-file'), seems to be non-blocking
+-- which may cause race conditions e.g. mpv trying to load a non-existent
+-- gdrive:// protocol before this script sets the correct url.
+mp.add_hook("on_load", 50, on_load_hook)
+
+
+
+-----------------------------
+-- GNARLY INTERNALS FOLLOW
+-----------------------------
+
 
 function set_auth_headers(access_token)
     print("Setting GDrive authorization headers")
@@ -8,33 +55,36 @@ function set_auth_headers(access_token)
 end
 
 
-
-function load_gdrive()
-    print("Loading GDrive script")
-
-    local url = mp.get_property("path", "")
-    if (url:find("gdrive://") == 1) then
-
-        print("Detected gdrive:// protocol, rewriting url")
-        url = "https://www.googleapis.com/drive/v3/files/" .. url:sub(10) .. "?alt=media"
-        mp.set_property("stream-open-filename", url)
-
-        set_auth_headers(TOKEN)
-    else
-        print("Not gdrive:// protocol, doing nothing.")
-    end
-
+function load_options()
+    local options = require 'mp.options'
+    local o = {
+        gdrive_client_id = "",
+        gdrive_client_secret = "",
+        gdrive_refresh_token = "",
+    }
+    options.read_options(o, "gdrive")
+    return o
 end
 
-function handle_load_fail()
-    -- In case token expires, refresh it (TODO)
-    print('failed, resetting...', TOKEN)
-    set_auth_headers(TOKEN)
-end
 
--- Apparently hooks are bad and unsupported but ytdl_hook uses them.
--- The alternative, register_event('start-file'), seems to be non-blocking
--- which may cause race conditions e.g. mpv trying to load a non-existent
--- gdrive:// protocol before this script sets the correct url.
-mp.add_hook("on_load", 50, load_gdrive)
-mp.add_hook("on_load_fail", 50, handle_load_fail)
+function get_access_token(o)
+    require("socket")
+    local https = require 'ssl.https'
+    local utils = require 'mp.utils'
+
+    request_body = (
+        "client_id=" .. o.gdrive_client_id ..
+        "&client_secret=" .. o.gdrive_client_secret ..
+        "&refresh_token=" .. o.gdrive_refresh_token ..
+        "&grant_type=refresh_token"
+    )
+    print("Requesting token for client_id", o.gdrive_client_id:sub(1, 10) .. "[...]")
+    local response_body, code, headers, status = https.request(
+        "https://www.googleapis.com/oauth2/v4/token",
+        request_body
+    )
+    local resp_json, err = utils.parse_json(response_body)
+    access_token = resp_json["access_token"]
+    print("Received access_token", access_token:sub(1, 10) .. "[...]")
+    return access_token
+end
